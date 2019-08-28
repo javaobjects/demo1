61afe55f00f9cdfa7452d1aea7aedbd1bb6157b8
yanbo0039 <yanbo0039@yeah.net>
2019-08-28 19:00:44 +0800

    UserServiceImpl finish

--- src/net/alibaba/demo1/service/impl/UserServiceImpl.java
+++ src/net/alibaba/demo1/service/impl/UserServiceImpl.java
@@ -1,13 +1,17 @@
 package net.alibaba.demo1.service.impl;
 
+import net.alibaba.demo1.dao.ifac.UserDaoIfac;
+import net.alibaba.demo1.dao.impl.UserDaoImpl;
 import net.alibaba.demo1.domain.Users;
 import net.alibaba.demo1.service.ifac.UserServiceIfac;
 
 public class UserServiceImpl implements UserServiceIfac{
+	
+	private UserDaoIfac userDao=new UserDaoImpl();
 
 	@Override
 	public boolean register(Users user) {
-		return false;
+		return userDao.addUser(user) > 0;
 	}
 
 }
