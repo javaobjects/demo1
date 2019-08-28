37c5f09ad5a4b03e374fc8e53469f620708028aa
yanbo0039 <yanbo0039@yeah.net>
2019-08-28 18:57:31 +0800

    UserServiceImpl

---
+++ src/net/alibaba/demo1/service/impl/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package net.alibaba.demo1.service.impl;
+
+import net.alibaba.demo1.domain.Users;
+import net.alibaba.demo1.service.ifac.UserServiceIfac;
+
+public class UserServiceImpl implements UserServiceIfac{
+
+	@Override
+	public boolean register(Users user) {
+		return false;
+	}
+
+}
